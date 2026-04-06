a0a329f4fd8f3bec3b27c77bc3da945a65d0a1a4
Rupa Dhillon <rupa211984@gmail.com>
2026-04-06 15:05:45 -0700

    (chore): set up deployment to GitHub Pages flow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/simplybudget',
+  assetPrefix: '/simplybudget',
+  images: {
+    unoptimized: true,
+  }
+};
 
 export default nextConfig;
